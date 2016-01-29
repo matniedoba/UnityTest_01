ce10bca05c04b8f41d5227261f4ff87c0dfcaa47
Mat Niedoba <matniedoba@gmail.com>
2016-01-29 23:15:58 +0100

    Last one

---
+++ UnityGitHubTest/Assets/Scripts/Hello.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hello : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Hellow World");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
